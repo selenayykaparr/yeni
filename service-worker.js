df6c06734219b10ef732326dcfe6678e257cf2a0
selenayykaparr <selenayy.kaparr@gmail.com>
2026-09-12 15:23:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // İnternetsiz ana ekran desteği
 // ==========================================
 
-const CACHE_NAME = "molly-co-v2";
+const CACHE_NAME = "molly-co-v3";
 
 const APP_SHELL = [
   "/",
